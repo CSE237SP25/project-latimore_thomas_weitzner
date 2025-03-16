6af46b3eeba4cea48bd8c47aaaaf0c7ed25d54d3
tegan <t.a.thomas@wustl.edu>
2025-03-16 12:31:22 -0500

    wrote up most of code, need to write tests

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -1,5 +1,77 @@
 package bankapp;
+import java.util.Scanner;
 
 public class Menu {
 
+	private Scanner inputScanner;
+	private BankAccount userAccount;
+
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		while (true) { 
+			menu.provideUserChoices();
+			String userInput = menu.getUserInput();
+			menu.processUserInput(userInput);
+		}
+	}
+
+	public Menu(){
+		this.inputScanner = new Scanner(System.in);
+		this.userAccount = new BankAccount();
+		System.out.println("Hello! Welcome to our bank app!");
+	}
+
+	public void provideUserChoices(){
+		System.out.println("Would you like to: ");
+		System.out.println("a.) Deposit money");
+		System.out.println("b.) Withdraw money");
+	}
+
+	public String getUserInput(){
+		return this.inputScanner.nextLine();
+	}
+
+	public void processUserInput(String userInput){
+		switch(userInput.toLowerCase()){
+			case "a":
+				deposit();
+				break;
+			case "b":
+				withdraw();
+				break;
+			default:
+				System.out.println("Invalid choice. Please try again.");
+		}
+	}
+
+	public void deposit(){
+		System.out.println("How much would you like to deposit?");
+		boolean validDeposit = false;
+		while (!validDeposit) { 
+			try {
+					double depositAmount = Double.parseDouble(getUserInput());
+					userAccount.deposit(depositAmount);
+					validDeposit = true;
+				} catch (IllegalArgumentException e) {
+					System.out.println("Invalid deposit amount. Please enter a deposit amount greater than or equal to 0.");
+				}
+			}
+		System.out.println("Your new balance is: " + userAccount.getCurrentBalance());
+	}
+
+	public void withdraw(){
+		System.out.println("How much would you like to withdraw?");
+		boolean validWithdraw = false;
+		while (!validWithdraw) { 
+			try {
+					double withdrawAmount = Double.parseDouble(getUserInput());
+					userAccount.withdraw(withdrawAmount);
+					validWithdraw = true;
+				} catch (IllegalArgumentException e) {
+					double currentBalance = userAccount.getCurrentBalance();
+					System.out.println("Invalid withdrawal amount. Please enter a positive withdrawal amount less than or equal to your current balance of: $" + currentBalance);
+				}
+			}
+		System.out.println("Your new balance is: " + userAccount.getCurrentBalance());
+	}
 }
