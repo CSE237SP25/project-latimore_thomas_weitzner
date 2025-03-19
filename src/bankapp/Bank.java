d0a7632b18d3d6d8fbf594d6ee9d090156962370
tegan <t.a.thomas@wustl.edu>
2025-03-19 09:42:34 -0500

    start basic stuff for this

--- src/bankapp/Bank.java
+++ src/bankapp/Bank.java
@@ -2,6 +2,9 @@ package bankapp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Bank {
 	//NOTE: This is the entire bank, should only be ONE BANK OBJECT IN THE PROJECT
@@ -22,6 +25,26 @@ public class Bank {
 	
 	public List<BankAccount> getAccounts() {
 		return this.accounts;
+	}
+
+	public void saveAccountsToFile() {
+		try (FileWriter writer = new FileWriter('bankPastInfo.txt')) {
+			for (BankAccount account : accounts) {
+				writer.write(account.getAccountHolderName() + "," + account.getAccountNumber() + "," + account.getCurrentBalance() + "\n");
+			}
+		} catch (IOException e) {
+			System.out.println("Error saving accounts to file: " + e.getMessage());
+		}
+	}
+
+	public void loadAccountsFromFile() {
+		Scanner scanPast = new Scanner(new File("bankPastInfo.txt"));
+		ArrayList<String> list = new ArrayList<String>();
+		while (s.hasNext()){
+    		list.add(s.next());
+		}
+		s.close();
+		//NEED TO IMPLEMENT THE REST OF THE LOGIC HERE
 	}
 	
 }
