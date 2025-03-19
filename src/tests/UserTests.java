faab3d0e7acf55cfa1e4f8260827fe07a9a7788c
PeytonRod <p.d.rodriguez@wustl.edu>
2025-03-19 11:11:36 -0500

    user class and user unit tests

---
+++ src/tests/UserTests.java
@@ -0,0 +1,61 @@
+package tests;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+import bankapp.BankAccount;
+import bankapp.User;
+
+public class UserTests {
+
+	//Testing creation of new user
+	
+	@Test
+	void newUserTest() {
+		User user1 = new User("BillyIsCool", "socks");
+		User user2 = new User("Username", "Password");
+		assertEquals("Username",user2.getUsername());
+		assertEquals("Password",user2.getPassword());
+		assertEquals("BillyIsCool", user1.getUsername());
+		assertEquals("socks", user1.getPassword());
+	}
+	
+	@Test
+	void addingAccountsTest(){
+		User user1 = new User("JohnSmith1", "JaneDoe!");
+		BankAccount newAccount = new BankAccount("SampleAccount");
+		BankAccount anotherAccount = new BankAccount("SecondSample");
+		user1.addAccount(newAccount);
+		assertEquals(1, user1.getAccounts().size());
+		user1.addAccount(anotherAccount);
+		assertEquals(2, user1.getAccounts().size());
+		
+	}
+	
+	@Test
+	void removingAccountsTest() {
+		User user1 = new User("JohnSmith1", "JaneDoe!");
+		BankAccount newAccount = new BankAccount("SampleAccount");
+		BankAccount anotherAccount = new BankAccount("SecondSample");
+		user1.addAccount(newAccount);
+		assertEquals(1, user1.getAccounts().size());
+		user1.addAccount(anotherAccount);
+		assertEquals(2, user1.getAccounts().size());
+		user1.removeAccount(anotherAccount);
+		assertEquals(1, user1.getAccounts().size());
+	}
+	@Test
+	void changePasswordTest() {
+		User user1 = new User("JohnSmith1", "JaneDoe!");
+		user1.changePassword("JaneDoe!", "MaryJane2");
+		assertEquals("MaryJane2", user1.getPassword());
+		assertEquals(false, user1.changePassword("JaneDoe!", "NewPassword"));
+		
+	}
+	
+	
+}
